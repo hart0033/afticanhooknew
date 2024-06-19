3b472e3a29f99e9e74056dfe0939fa5f145dd979
mhart001 <hartmichael507@gmail.com>
2024-06-19 13:10:53 -0700

    update

---
+++ hartlord/naijahook/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Report, postads, slide,slide2,slide3,review,adsvideos, verify_Post, verify_video, videosreview, State, Service
+# Register your models here.
+admin.site.register(postads)
+admin.site.register(slide)
+admin.site.register(slide2)
+admin.site.register(slide3)
+admin.site.register(review)
+admin.site.register(videosreview)
+admin.site.register(adsvideos)
+admin.site.register(State)
+admin.site.register(Service)
+admin.site.register(Report)
+admin.site.register(verify_Post)
+admin.site.register(verify_video)
